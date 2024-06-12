a776bdc1a996a716caaebd1c066aac6e52ff9b03
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-06-12 21:12:50 +0200

    Add setup styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  primary: '#415385',
+
+  black: '#242424',
+  blue: '#415385',
+  dark: '#2b2f34',
+  light: '#e4e4e4',
+  gray: '#e5e5e5',
+  gray2: '#cccbcb',
+  green: '#389e0d',
+  warning: '#C52A1A',
+  white: '#FFFFFF',
+};
