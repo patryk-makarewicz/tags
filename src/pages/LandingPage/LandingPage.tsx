1b553f573273721ba13bf725c26d99c7ab06d69b
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-06-13 10:56:04 +0200

    Add checkbox to form

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,26 +1,25 @@
-import { useRef } from 'react';
+import { useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-import { useGetSearchTags, useSaveTags, useGetSavedTags, useDeleteTag } from '../../hooks';
+import { useGetSearchTags, useSaveTags, useGetSavedTags, useDeleteTag, useDebounce } from '../../hooks';
 import { PageLayout } from '../../layout';
 
 type SearchInput = {
   query: string[];
+  search: string;
 };
 
 export const LandingPage = () => {
   const { t } = useTranslation();
 
-  const searchRef = useRef<HTMLInputElement>(null);
-
   const { data: savedTags } = useGetSavedTags();
-  const { data: searchTags, refetch } = useGetSearchTags(searchRef.current?.value);
+
   const { mutate: saveTags } = useSaveTags();
   const { mutate: deleteTag } = useDeleteTag();
 
-  const { register, handleSubmit } = useForm<SearchInput>();
-  const onSubmit: SubmitHandler<SearchInput> = (data) =>
+  const { register, handleSubmit, watch, setValue } = useForm<SearchInput>();
+  const onSubmit: SubmitHandler<SearchInput> = (data) => {
     saveTags({
       records: data.query.map((tag) => ({
         fields: {
@@ -29,30 +28,46 @@ export const LandingPage = () => {
       })),
     });
 
+    setValue('search', '');
+  };
+
+  const searchWatch = useDebounce(watch('search'), 300);
+  const { data: searchTags, refetch } = useGetSearchTags(searchWatch);
+
+  useEffect(() => {
+    if (searchWatch?.length > 0) {
+      refetch();
+    }
+  }, [searchWatch]);
+
   return (
     <PageLayout>
       <h2>{t('hello')}</h2>
       <div>
-        <h3>Tags list to select:</h3>
-        <input
-          type="text"
-          ref={searchRef}
-          defaultValue=""
-          placeholder="Search"
-          autoComplete="off"
-          onChange={() => refetch()}
-        />
-      </div>
-      <div>
-        <h4>Tags list to select:</h4>
         <form onSubmit={handleSubmit(onSubmit)}>
-          {searchTags.map((tag) => (
-            <div key={tag.id}>
-              <input type="checkbox" value={tag.fields.name} {...register('query')} />
-              <label>{tag.fields.name}</label>
-            </div>
-          ))}
-          <button type="submit">Zapisz</button>
+          <div>
+            <h3>Tags list to select:</h3>
+            <input type="text" defaultValue="" placeholder="Search" autoComplete="off" {...register('search')} />
+          </div>
+          <div>
+            {searchWatch?.length > 0 && (
+              <div>
+                <h4>Tags list to select:</h4>
+                {searchTags.map((tag) => {
+                  const selectedTag = savedTags.some((saved) => saved.fields.name === tag.fields.name);
+
+                  return (
+                    <div key={tag.id}>
+                      <input type="checkbox" value={tag.fields.name} disabled={selectedTag} {...register('query')} />
+                      <label>{tag.fields.name}</label>
+                    </div>
+                  );
+                })}
+
+                <button type="submit">Zapisz</button>
+              </div>
+            )}
+          </div>
         </form>
       </div>
       <div>
