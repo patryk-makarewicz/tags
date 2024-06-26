7ab036327313b26a95d2ca905f6ab33c7ae2c9bf
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2024-06-26 22:41:55 +0200

    Add Toast on actions

--- src/hooks/useSaveTags.ts
+++ src/hooks/useSaveTags.ts
@@ -1,21 +1,24 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
 
 import { QueryKey, TagsAPI } from '@/api';
 import { SaveTagModel } from '@/api/Tags';
 
 export const useSaveTags = () => {
+  const { t } = useTranslation();
   const queryClient = useQueryClient();
 
   const { mutate, isSuccess, isError, isPending } = useMutation<SaveTagModel, AxiosError, SaveTagModel>({
     mutationFn: (data: SaveTagModel) => TagsAPI.saveTags(data),
     mutationKey: [QueryKey.saveTags],
     onSuccess: () => {
+      toast.success(t('messages.saveTag'));
       queryClient.invalidateQueries({ queryKey: [QueryKey.getSavedTags] });
-      //TODO: add success notification
     },
     onError: (error: AxiosError) => {
-      //TODO: add error notification
+      toast.error(t('messages.SaveTagFailed'));
       console.error('Error:', error.message);
     },
   });
